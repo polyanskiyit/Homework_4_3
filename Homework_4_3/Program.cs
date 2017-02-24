a411b3be86e711fc25f3023d73df78c575663c53
polyanskiyit <polyanskiyit@gmail.com>
2017-02-24 23:32:05 +0200

    2

--- Homework_4_3/Program.cs
+++ Homework_4_3/Program.cs
@@ -1,37 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homework_4_3
 {
     class Program
     {
-
-        private static void arithmeticProgression(ref double[,] arr, int maxRow, int maxCol, int number)
+        private static void arithmeticProgression(ref double[,] arr, int maxRow, int maxCol)
         {
-
+            int number = 0;
             for (int i = 0; i < maxRow; i++)
             {
                 for (int j = 0; j < maxCol; j++)
                 {
                     arr[i, j] = ++number;
-                    Console.Write(+arr[i, j] + " ");
+                    Console.Write(arr[i, j] + " ");
                 }
                 Console.WriteLine();
             }
         }
 
-
         static void Main(string[] args)
         {
             int maxRow = 4;
             int maxCol = 4;
-            int number = 0;
             double[,] arr = new double[maxRow, maxCol];
             
-            arithmeticProgression(ref arr, maxRow, maxCol, number);
+            arithmeticProgression(ref arr, maxRow, maxCol);
 
 
             Console.Read();
